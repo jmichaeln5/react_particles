ac5ffd95e43d80be17cffe382f35b51e4e56eba6
Jon Norton <jmichaeln5@gmail.com>
2022-03-15 10:49:25 -0400

    adding pre compile on initialization and auto mounting to /react_particles in root app

--- lib/react_particles.rb
+++ lib/react_particles.rb
@@ -1,6 +1,16 @@
 require "react_particles/version"
 require "react_particles/engine"
 
+
+# Everything to be shipped w engine
 module ReactParticles
   # Your code goes here...
+  # module Controllers # Example from Devise
+  #   # autoload :Helpers,        'react_particles/controllers/helpers'
+  #   # autoload :Rememberable,   'react_particles/controllers/rememberable'
+  #   # autoload :ScopedViews,    'react_particles/controllers/scoped_views'
+  #   # autoload :SignInOut,      'react_particles/controllers/sign_in_out'
+  #   # autoload :StoreLocation,  'react_particles/controllers/store_location'
+  #   # autoload :UrlHelpers,     'react_particles/controllers/url_helpers'
+  # end
 end
--- lib/react_particles/engine.rb
+++ lib/react_particles/engine.rb
@@ -1,5 +1,32 @@
 module ReactParticles
   class Engine < ::Rails::Engine
     isolate_namespace ReactParticles
+
+    ## Precompiling assets
+    ## Article:
+      ## https://jakeyesbeck.com/2016/03/20/how-to-build-a-ruby-on-rails-engine/
+        ## Find in page:
+        ## To enable precompiled assets, a few more lines need to be added in the same engine.rb file:
+    initializer("react_particles.assets.precompile") do |app|
+      app.config.assets.precompile += [
+          # 'application.css', # from article
+          # 'application.js' # from article
+          'react_particles/application.css', # Personally entered for namespace
+          # 'react_particles/application.js' # Personally entered for namespace
+        ]
+    end
+
+    ## Self mounting with namespace
+    ## Article:
+      ## https://jakeyesbeck.com/2016/03/20/how-to-build-a-ruby-on-rails-engine/
+        ## Find in page:
+        ## Alternatively, an Engine can mount itself using the same initializer method in engine.rb:
+    initializer('react_particles',
+            after: :load_config_initializers) do |app|
+      Rails.application.routes.prepend do
+        mount Passages::Engine, at: '/react_particles'
+      end
+    end
+
   end
 end
