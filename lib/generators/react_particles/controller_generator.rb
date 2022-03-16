9e610a4101eb17e5dc41189d93ea1d255bf69ec2
Jon Norton <jmichaeln5@gmail.com>
2022-03-15 22:50:40 -0400

    Fixing typo

--- lib/generators/react_particles/controller_generator.rb
+++ lib/generators/react_particles/controller_generator.rb
@@ -1,7 +1,7 @@
 module ReactParticles
   module Generators
     class ControllerGenerator < Rails::Generators::NamedBase
-      include Rails::Generators::ResourceHelper
+      include Rails::Generators::ResourceHelpers
 
       desc "Generates controller, controller_spec and views for the model with the given NAME."
     end
