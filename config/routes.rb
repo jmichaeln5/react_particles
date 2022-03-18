b7c0e316c43dbd2394a5b641701d16128c97c625
Jon Norton <jmichaeln5@gmail.com>
2022-03-17 21:44:33 -0400

    Adding comments for routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,17 @@ ReactParticles::Engine.routes.draw do
   root to: 'components#root'
   get 'components/index'
 end
+
+#### Routing Article
+# https://devblast.com/b/rails-5-routes-scope-vs-namespace
+# scope module: 'admin' do
+#   resources :users
+# end
+
+# Prefix Verb   URI Pattern          Controller#Action
+#  users GET    /users(.:format)     admin/users#index
+#        POST   /users(.:format)     admin/users#create
+#   user GET    /users/:id(.:format) admin/users#show
+#        PATCH  /users/:id(.:format) admin/users#update
+#        PUT    /users/:id(.:format) admin/users#update
+#        DELETE /users/:id(.:format) admin/users#destroy
