7b042e6845afdbbb0940269cf5c8d3eef6abf420
Jon Norton <jmichaeln5@gmail.com>
2022-03-29 19:03:35 -0400

    Adding jbbundling generator w dir/file structure- Broad outline but displays when running rails generate to view available generate tasks

---
+++ lib/generators/react_particles/jsbundling/install_generator.rb
@@ -0,0 +1,14 @@
+require "rails/generators/base"
+require "react_particles/generator_helpers"
+require "react_particles/namespace"
+
+module ReactParticles
+  module Generators
+    module Jsbundling
+      class Install < Rails::Generators::Base
+        include ReactParticles::GeneratorHelpers
+
+      end
+    end
+  end
+end
