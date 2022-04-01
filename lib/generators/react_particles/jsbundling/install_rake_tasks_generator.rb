56bec18736aabc8404f68d9f3fb9e0076bbbdfe1
Jon Norton <jmichaeln5@gmail.com>
2022-04-01 14:12:53 -0400

    Adding lib/generators/react_particles/jsbundling/install_rake_tasks_generator.rb to install all js_bundling rake tasks

---
+++ lib/generators/react_particles/jsbundling/install_rake_tasks_generator.rb
@@ -0,0 +1,33 @@
+require "rails/generators/base"
+require "react_particles/generator_helpers"
+require "react_particles/namespace"
+
+module ReactParticles
+  module Generators
+    module Jsbundling
+      class InstallRakeTasksGenerator < Rails::Generators::Base
+        include ReactParticles::GeneratorHelpers
+
+        class_option :namespace, type: :string, default: "react_application"
+        class_option :js_bundler, type: :string, default: "esbuild"
+
+        def run_jsbundling_rake_task_generators
+          call_generator("react_particles:jsbundling:build")
+          call_generator("react_particles:jsbundling:clobber")
+          call_generator("react_particles:jsbundling:output_js_bundler")
+        end
+
+        private
+
+          def namespace
+            options[:namespace]
+          end
+
+          def js_bundler
+            options[:js_bundler]
+          end
+
+      end
+    end
+  end
+end
