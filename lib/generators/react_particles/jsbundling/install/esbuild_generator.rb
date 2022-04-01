d97ad9c422e6661668c79316b0f164ba0d01dd41
Jon Norton <jmichaeln5@gmail.com>
2022-04-01 14:04:40 -0400

    Added nested install dir in jsbundling dir, to match official jsbundling-rails commands with prepending react_particles

---
+++ lib/generators/react_particles/jsbundling/install/esbuild_generator.rb
@@ -0,0 +1,16 @@
+require "rails/generators/base"
+require "react_particles/generator_helpers"
+require "react_particles/namespace"
+
+module ReactParticles
+  module Generators
+    module Jsbundling
+      module Install
+        class EsbuildGenerator < Rails::Generators::Base
+        include ReactParticles::GeneratorHelpers
+
+        end
+      end
+    end
+  end
+end
