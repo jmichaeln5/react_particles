3850b86ce2688c5e82468f39c4829bde6da0af34
Jon Norton <jmichaeln5@gmail.com>
2022-04-06 10:19:22 -0400

    Creating package.json via template in esbuild generator

--- lib/generators/react_particles/jsbundling/install/esbuild_generator.rb
+++ lib/generators/react_particles/jsbundling/install/esbuild_generator.rb
@@ -32,36 +32,90 @@ module ReactParticles
             end
           end
 
-          def create_package_json
-            if self.behavior == :invoke
-              esbuild_package_json = "#{javascript_dir_path}/package.json"
-              `touch #{esbuild_package_json}`
+          ################################################################################################################
+          ################################################################################################################
+          ################################################################################################################
+          ### NOTE CHOOSE WHICH STYLE OF INSTALL, USE AMONGST ALL JSBUNDLER INSTALL GENERATORS
+          ### NOTE CHOOSE WHICH STYLE OF INSTALL, USE AMONGST ALL JSBUNDLER INSTALL GENERATORS
+          ### NOTE CHOOSE WHICH STYLE OF INSTALL, USE AMONGST ALL JSBUNDLER INSTALL GENERATORS
+          ############################ Installs esbuild with JSON template file
+          def generate_package_json
+            template_file = "package_json_template.json.erb"
+            generated_template_file = "#{javascript_dir_path}/package.json"
 
-              esbuild_config_json ="
-              \n{" +
-              "\n  \"name\": \"#{namespace}\", " +
-              "\n  \"private\": \"true\"" +
-              "\n}"
-
-              append_to_file(
-                esbuild_package_json,
-                esbuild_config_json,
-              )
+            case self.behavior
+            when :invoke
+              unless (Rails.root.join(template_file)).exist?
+                template(
+                  template_file,
+                  generated_template_file,
+                )
+              end
+            when :revoke
+              puts indent_str("removed ".red) + generated_template_file
+              `rm -rf #{javascript_dir_path}`
             end
           end
 
-          def install_js_bundler
-            if self.behavior == :invoke
-              ### Install Script from jsbundling-rails 👇🏾
-              ### /jsbundling-rails/lib/install/esbuild/install.rb
-              install_script = File.expand_path("../esbuild_templates/install.rb", __FILE__)
+          def install_react_es_build
+            generated_template_file = "#{javascript_dir_path}/package.json"
 
-              Dir.chdir(javascript_dir_path) do
-                # Install Script from jsbundling-rails 👇🏾
-                system "ruby #{install_script}"
+            case self.behavior
+            when :invoke
+              if (Rails.root.join(generated_template_file)).exist?
+                Dir.chdir "#{javascript_dir_path}" do
+                    # system 'yarn add react react-dom esbuild'
+                    system 'yarn add esbuild'
+                    system 'yarn run build'
+                end
+              end
+            when :revoke
+              if ( generated_json_file_path = Rails.root.join(generated_template_file)).exist?
+                Dir.chdir "#{javascript_dir_path}" do
+                  # system 'yarn remove react react-dom esbuild'
+                  system 'yarn remove esbuild'
+                end
+                `rm -rf #{javascript_dir_path}` if (Dir.exists? javascript_dir_path) and (Dir.empty? javascript_dir_path)
               end
             end
           end
+          ### NOTE CHOOSE WHICH STYLE OF INSTALL, USE AMONGST ALL JSBUNDLER INSTALL GENERATORS
+          ### NOTE CHOOSE WHICH STYLE OF INSTALL, USE AMONGST ALL JSBUNDLER INSTALL GENERATORS
+          ### NOTE CHOOSE WHICH STYLE OF INSTALL, USE AMONGST ALL JSBUNDLER INSTALL GENERATORS
+          ############################ Installs esbuild with jsbundling install script and manually configured JSON
+          # def create_package_json
+          #   if self.behavior == :invoke
+          #     esbuild_package_json = "#{javascript_dir_path}/package.json"
+          #     `touch #{esbuild_package_json}`
+          #
+          #     esbuild_config_json ="
+          #     \n{" +
+          #     "\n  \"name\": \"#{namespace}\", " +
+          #     "\n  \"private\": \"true\"" +
+          #     "\n}"
+          #
+          #     append_to_file(
+          #       esbuild_package_json,
+          #       esbuild_config_json,
+          #     )
+          #   end
+          # end
+          #
+          # def install_js_bundler
+          #   if self.behavior == :invoke
+          #     ### Install Script from jsbundling-rails 👇🏾
+          #     ### /jsbundling-rails/lib/install/esbuild/install.rb
+          #     install_script = File.expand_path("../esbuild_templates/install.rb", __FILE__)
+          #
+          #     Dir.chdir(javascript_dir_path) do
+          #       # Install Script from jsbundling-rails 👇🏾
+          #       system "ruby #{install_script}"
+          #     end
+          #   end
+          # end
+          ################################################################################################################
+          ################################################################################################################
+          ################################################################################################################
 
           def revoke_callbacks
             if self.behavior == :revoke
