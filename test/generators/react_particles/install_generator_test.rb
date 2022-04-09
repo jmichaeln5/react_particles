c4b58ef719136fe332029226fee48d422ca5a98e
Jon Norton <jmichaeln5@gmail.com>
2022-04-09 01:14:28 -0400

    Removing unnecessary require, HOST_ROOT loaded in test_helper

--- test/generators/react_particles/install_generator_test.rb
+++ test/generators/react_particles/install_generator_test.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "test_helper"
-require "#{ENGINE_PATH}"
 
 class ReactParticlesInstallGeneratorTest < Rails::Generators::TestCase
   tests ReactParticles::Generators::InstallGenerator
